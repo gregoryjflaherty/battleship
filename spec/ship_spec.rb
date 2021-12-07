5ce4092f20caa7f1f038fe6dba3b0c08d906c808
Austin Moore <austinchristianmoore@gmail.com>
2021-12-06 20:09:30 -0500

    Test: Ship has a name

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -4,5 +4,14 @@ RSpec.describe Ship do
 
   it 'exists' do
     cruiser = Ship.new("Cruiser", 3)
+
     expect(cruiser).to be_an_instance_of (Ship)
   end
+
+  it 'has a name' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+end
