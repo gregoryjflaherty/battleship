60a3508612b5b450e08364894ea47f3f64999fc5
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-06 23:02:18 -0500

    Test: it exists

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,2 +1,10 @@
 require './lib/cell'
 require './lib/ship'
+
+RSpec.describe Cell do
+  it 'exists with arguments' do
+    cell = Cell.new('B4')
+    
+    expect(cell).to be_an_instance_of(Cell)
+  end
+end
