8b4a19e32385bb4bc3cec6c5db588194f0cdf1e4
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-06 23:45:58 -0500

    Test: can show if has ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -59,4 +59,13 @@ RSpec.describe Cell do
 
     expect(cell_1.render).to eq('M')
   end
+
+  it 'can show if has ship' do
+    cell_2 = Cell.new('C3')
+    cruiser = Ship.new('Cruiser', 3)
+    cell_2.place_ship(cruiser)
+    
+    expect(cell_2.render).to eq('.')
+    expect(cell_2.render(true)).to eq('S')
+  end
 end
