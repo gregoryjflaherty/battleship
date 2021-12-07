2f2c69b60094a61ded0531c2fc98889cd43b7208
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-06 23:52:49 -0500

    Test: can show if sunk

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -78,4 +78,16 @@ RSpec.describe Cell do
     expect(cell_2.render).to eq('H')
     expect(cruiser.sunk?).to eq(false)
   end
+
+  it 'can show if sunk' do
+    cell_2 = Cell.new('C3')
+    cruiser = Ship.new('Cruiser', 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    cruiser.hit
+    cruiser.hit
+
+    expect(cell_2.render).to eq('X')
+    expect(cruiser.sunk?).to eq(true)
+  end
 end
