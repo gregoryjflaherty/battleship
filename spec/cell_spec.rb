72d85e4688ba4db9d01d43e76a8ff48434a3a913
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-06 23:05:06 -0500

    Test: it has a coordinate

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,7 +4,13 @@ require './lib/ship'
 RSpec.describe Cell do
   it 'exists with arguments' do
     cell = Cell.new('B4')
-    
+
     expect(cell).to be_an_instance_of(Cell)
+  end
+
+  it 'has a coordinate' do
+    cell = Cell.new('B4')
+
+    expect(cell.coordinate).to eq('B4')
   end
 end
