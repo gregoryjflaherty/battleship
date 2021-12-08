652ca857a02f37b95e0a3d6c4e3925581aa869b7
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-08 09:52:35 -0500

    Test: validate consecutive coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,4 +28,14 @@ RSpec.describe Board do
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(false)
   end
+
+  it 'can validate coordinates are consecutive'  do
+    board = Board.new
+    submarine = Ship.new("Submarine", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to be(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
+  end
 end
