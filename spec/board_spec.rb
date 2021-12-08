14466eb3f540e7648cce6f9b130e977c5b81ffa2
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-07 23:47:36 -0500

    Test: it has cells test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_an_instance_of(Board)
   end
+
+  it 'has cells' do
+    board = Board.new
+    expect(board.cells).to be_a(Hash)
+    expect(board.cells.size).to be(16)
+  end
 end
