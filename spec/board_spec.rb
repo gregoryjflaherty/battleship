c91f8d9b5a46b392aa0a5956b0d3f2a796a0792e
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-07 23:50:55 -0500

    Test: it can validate coordinates test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Board do
     expect(board.cells).to be_a(Hash)
     expect(board.cells.size).to be(16)
   end
+
+  it 'can validate coordinate' do
+    board = Board.new
+    expect(board.valid_coordinate?("A1")).to be(true)
+    expect(board.valid_coordinate?("D4")).to be(true)
+    expect(board.valid_coordinate?("A5")).to be(false)
+    expect(board.valid_coordinate?("E1")).to be(false)
+    expect(board.valid_coordinate?("A22")).to be(false)
+  end
 end
