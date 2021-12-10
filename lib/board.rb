d485eacd289e60fc828814eb552be5d302066150
Austin Moore <austinchristianmoore@gmail.com>
2021-12-09 20:43:34 -0500

    Feature: Create equal_letters? and nums_increase? methods

--- lib/board.rb
+++ lib/board.rb
@@ -20,12 +20,25 @@ class Board
   end
 
   def valid_placement?(ship, coord_array)
-    if coord_array.length == ship.length
-      true
+    if coord_array.length == ship.length && equal_letters?(coord_array)
+      nums_increase_1(coord_array)
+    elsif coord_array.length == ship.length
+      #
+      #
+        #
+        #
     else
-      false
+      false #Ship length is not correct
     end
-    
+  end
+
+  def equal_letters?(coord_array)
+    coord_array.all? {|coord|coord[0] == coord_array[0][0]}
+  end
+
+  def nums_increase_1?(coord_array)
+    compare = coord_array.map {|coord| coord[-1].to_i} #fails if coordinate>12
+    compare.each_cons(2).all? {|x,y| y == x+1}
   end
 
 end
