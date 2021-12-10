b5abeedb9c7ed3ae39002842b9821a7c50d38098
Austin Moore <austinchristianmoore@gmail.com>
2021-12-09 21:08:30 -0500

    Feature: Pass placement tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require './lib/cell'
 require './lib/ship'
 
@@ -21,12 +20,9 @@ class Board
 
   def valid_placement?(ship, coord_array)
     if coord_array.length == ship.length && equal_letters?(coord_array)
-      nums_increase_1(coord_array)
+      nums_increase_1?(coord_array)
     elsif coord_array.length == ship.length
-      #
-      #
-        #
-        #
+      valid_vertical?(coord_array) #If true, then nums_identical && letters_increase_1
     else
       false #Ship length is not correct
     end
@@ -37,8 +33,22 @@ class Board
   end
 
   def nums_increase_1?(coord_array)
-    compare = coord_array.map {|coord| coord[-1].to_i} #fails if coordinate>12
+    compare = coord_array.map {|coord| coord[-1].to_i} #error if coordinate>12
     compare.each_cons(2).all? {|x,y| y == x+1}
+  end
+
+  def nums_identical?(coord_array)
+    compare = coord_array.map {|coord| coord[-1].to_i} #bug if coordinate>12
+    compare.each_cons(2).all? {|x,y| y == x}
+  end
+
+  def letters_increase_1?(coord_array)
+    compare = coord_array.map {|coord| coord[0].ord} #bug if coordinate>z?
+    compare.each_cons(2).all? {|x,y| y == x+1}
+  end
+
+  def valid_vertical?(coord_array)
+    nums_identical?(coord_array) == true ? letters_increase_1?(coord_array) : false
   end
 
 end
