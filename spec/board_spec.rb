cd7426aff559fadd2a441359acda53cf2ac06fbe
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-10 15:34:52 -0500

    Test: create places same cruiser test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -81,12 +81,25 @@ RSpec.describe Board do
     expect(cell_5.ship).to be(submarine)
   end
 
-  it 'places the same ship in every cell' do
+  it 'places the same submarine in every cell' do
     board = Board.new
     submarine = Ship.new("Submarine", 2)
     cell_4 = board.cells["C1"]
     cell_5 = board.cells["D1"]
+    board.place(submarine, ["C1", "D1"])
 
     expect(cell_4.ship).to eq(cell_5.ship)
+  end
+
+  it 'places the same cruiser in every cell'  do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    expect(cell_1.ship).to eq(cell_2.ship)
+    expect(cell_2.ship).to eq(cell_3.ship)
   end
 end
