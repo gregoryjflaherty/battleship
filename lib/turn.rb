249800d6f2d3547dd2b93a8f62d79a429a56af69
Austin Moore <austinchristianmoore@gmail.com>
2021-12-11 18:23:26 -0500

    Feature: Create setup method

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,10 +5,27 @@ class Turn
     @play = play
   end
 
+    def setup
+      @user_cruiser   = Ship.new("Cruiser", 3)
+      @user_submarine = Ship.new("Submarine", 2)
+      @comp_cruiser   = Ship.new("Cruiser", 3)
+      @comp_submarine = Ship.new("Submarine", 2)
+      @computer       = Computer.new
+      @user_board     = Board.new
+
+      @computer.assign_to_cells(@comp_cruiser) #Random assignment
+      @computer.assign_to_cells(@comp_submarine) #Random assignment
+      #
+    end
+
   def welcome
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
     @play = gets.chomp
+  end
+
+  def
+
   end
 
 end
