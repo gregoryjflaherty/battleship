6d97ff812f245daa90541206fd5e9e19b824f7de
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-11 15:11:16 -0500

    Feat: create horizontal array method

--- lib/comp.rb
+++ lib/comp.rb
@@ -1,6 +1,8 @@
 require './lib/board'
 
 class Computer
+  attr_reader :board, :cruiser, :submarine
+
   def initialize
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
@@ -13,9 +15,19 @@ class Computer
 
   def random_direction
     rand_num = rand(1..2)
-    random_direction == 1 ? :horizontal : :vertical
+    rand_num == 1 ? :horizontal : :vertical
+  end
+
+
+  def horizontal_array(random_coordinate, ship)
+    coordinates = []
+    coordinate = random_coordinate
+    ship.length.times do |i|
+      coordinates << "#{coordinate[0]}#{coordinate[1].to_i + i}"
+    end
+    coordinates
   end
 end
-comp = Computer.new
-p comp.random_coordinate
-p comp.random_direction
+
+#comp = Computer.new
+#p comp.horizontal_array(comp.random_coordinate, comp.cruiser )
