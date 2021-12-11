bd6c8bafbcb48dbd23cbd203cb9e7814b8695a9c
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-11 14:58:50 -0500

    Feat: Create random direction method

--- lib/comp.rb
+++ lib/comp.rb
@@ -10,6 +10,12 @@ class Computer
   def random_coordinate
     @board.cells.values.sample.coordinate
   end
+
+  def random_direction
+    rand_num = rand(1..2)
+    random_direction == 1 ? :horizontal : :vertical
+  end
 end
 comp = Computer.new
 p comp.random_coordinate
+p comp.random_direction
