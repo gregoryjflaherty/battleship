4eba141d066aa02a4a6dd81c5eb6507c7a58d8be
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-15 16:47:24 -0500

    Refactor: remove unnecessary test

--- spec/comp_spec.rb
+++ spec/comp_spec.rb
@@ -2,20 +2,13 @@ require "./lib/ship"
 require "./lib/cell"
 require "./lib/board"
 require "./lib/comp"
-require "./lib/turn"
 
-RSpec.describe Computer do #Check class name with Greg
+
+RSpec.describe Computer do
   it 'exists' do
     computer = Computer.new
 
     expect(computer).to be_an_instance_of(Computer)
-  end
-
-  it 'has ships' do
-    computer = Computer.new
-
-    expect(computer.cruiser).to be_an_instance_of(Ship)
-    expect(computer.submarine).to be_an_instance_of(Ship)
   end
 
   it 'has a board' do
@@ -27,10 +20,13 @@ RSpec.describe Computer do #Check class name with Greg
 
   it 'generates ship coordinates' do
     computer  = Computer.new
-    expect(computer.rand_place_ship(computer.cruiser).length).to be(3)
-    expect(computer.board.valid_placement?(computer.cruiser, (computer.rand_place_ship(computer.cruiser)))).to be(true)
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
 
-    expect(computer.rand_place_ship(computer.submarine).length).to be(2)
-    expect(computer.board.valid_placement?(computer.submarine, (computer.rand_place_ship(computer.submarine)))).to be(true)
+    expect(computer.rand_place_ship(cruiser).length).to be(3)
+    expect(computer.board.valid_placement?(cruiser, (computer.rand_place_ship(cruiser)))).to be(true)
+
+    expect(computer.rand_place_ship(submarine).length).to be(2)
+    expect(computer.board.valid_placement?(submarine, (computer.rand_place_ship(submarine)))).to be(true)
   end
 end
