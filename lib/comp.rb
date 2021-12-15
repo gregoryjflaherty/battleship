254e2f509e2d7a2081d0b3d832337899aa7f7fae
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-15 07:35:14 -0500

    Feat: Create intelligent shot pattern

--- lib/comp.rb
+++ lib/comp.rb
@@ -1,13 +1,20 @@
 require './lib/board'
 
 class Computer
-  attr_reader :board, :cruiser, :submarine, :shot_sequence
+  attr_reader :board,
+              :cruiser,
+              :submarine,
+              :shot_sequence,
+              :last_hit,
+              :next_hit
 
   def initialize(length, height)
     @board = Board.new(length, height)
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @shot_sequence = diagonal_up_array
+    @last_hit = "A3"
+    @next_hit =
   end
 
   def random_coordinate  #4
@@ -26,7 +33,6 @@ class Computer
       horizontal_array(random_coordinate, ship.length)
     end
   end
-
 
   def horizontal_array(rand_coordinate, ship_length) ## 3rd
     coordinates = []
@@ -77,20 +83,102 @@ class Computer
     diagonal.map {|array| array.join}
   end
 
+  def vertical_sink_pattern(board)
+    coord_array = []
+    ord = @last_hit[0].ord + 1
+    coord_array = ["#{ord.chr}#{@last_hit[-1]}", "#{(ord - 2).chr}#{@last_hit[-1]}"]
+    coord_array.map {|coord| coord_array.delete(coord) if board.cells.keys.include?(coord) == false}
+    coord_array
+  end
+
+  def horizontal_sink_pattern(board)
+    num = @last_hit[-1].to_i
+    coord_array = ["#{@last_hit[0]}#{num - 1}", "#{@last_hit[0]}#{num + 1}"]
+    board.cells[coord_array[0]].fire_upon
+
+
+    #coord_array.map {|coord| coord_array.delete(coord) if board.cells.keys.include?(coord) == false}
+    #coord_array
+  end
+
+
   def start_intelligent_shots(board)
-    @shot_sequence.shuffle!
-    board.cells[@shot_sequence.first].fire_upon
-    @shot_sequence.delete(@shot_sequence.first)
+    shot = @shot_sequence.shuffle!.first
+    board.cells[shot].fire_upon
+    @last_hit = shot if board.cells[shot].empty? == false
+    @shot_sequence.delete(shot)
   end
 end
 
+user_board = Board.new
+computer_board = Computer.new(user_board.length, user_board.height)
+user_submarine = Ship.new("Submarine", 2)
+user_board.place(user_submarine, ["B3", "B4"])
+p computer_board.start_intelligent_shots(user_board)
+p computer_board.shot_sequence
+
+#p computer_board.vertical_sink_pattern(user_board)
+puts user_board.render
+p computer_board.last_hit
+p computer_board.horizontal_sink_pattern(user_board)
+p computer_board.vertical_sink_pattern(user_board)
+
+
+
+
+
 =begin
 user = Board.new
-p user.length
-p user.height
 computer = Computer.new(user.length, user.height)
+#computer.horizontal_sink_pattern(user)
+p computer.vertical_sink_pattern(user)
 
-p computer.shot_sequence
-p computer.start_intelligent_shots(user)
-p computer.shot_sequence
+
+  def attack(board)
+    start_intelligent_shots(board) if @last_hit == nil
+  end
+
+  def sink_the_ship(board)
+    if horizontal_sink_pattern.length.zero?
+      #vertical
+    else
+      horizontal_sink_pattern[]
+  end
+
+  def horizontal_shots(coord_array, board)
+
+  end
+
+
+  def horizontal_sink_pattern(board)
+    coord_array = ["#{@last_hit[0]}#{num}", "#{@last_hit[0]}#{num -2}"]
+    coord_array.map {|coord| coord_array.delete(coord) if board.cells.keys.include?(coord) == false}
+
+
+  end
+
+  def vertical_sink_pattern(board)
+    coord_array = []
+    ord = @last_hit[0].ord + 1
+    coord_array = ["#{ord.chr}#{@last_hit[-1]}", "#{(ord - 2).chr}#{@last_hit[-1]}"]
+    coord_array.map {|coord| coord_array.delete(coord) if board.cells.keys.include?(coord) == false}
+    coord_array
+  end
+=end
+=begin
+def horizontal_shots(board)
+  if @next_hit == nil
+    @next_hit = "#{@last_hit[0]}#{@last_hit[-1].to_i + 1}" ## EX A3
+  end
+  board.cells[@next_hit].fire_upon
+  if board.cells[@next_hit].ship == nil
+    if board.cells["#{@last_hit[0]}#{@last_hit[-1].to_i - 1}"].fired_upon? #A2
+      @next_hit = "#{@last_hit[0]}#{@last_hit[-1].to_i - 2}" #A1
+    else
+      @next_hit = "#{@last_hit[0]}#{@last_hit[-1].to_i - 1}" #A2
+    end
+  else
+    @last_hit = @next_hit #might need to reverse this
+  end
+end
 =end
--- lib/turn.rb
+++ lib/turn.rb
@@ -28,12 +28,8 @@ class Turn
     @user_submarine = Ship.new("Submarine", 2)
     @comp_cruiser   = Ship.new("Cruiser", 3)
     @comp_submarine = Ship.new("Submarine", 2)
-    #@computer       = Computer.new
-    #@user_board     = Board.new
-
     @computer.assign_to_cells(@comp_cruiser) #Random assignment
     @computer.assign_to_cells(@comp_submarine) #Random assignment
-    #
   end
 
   def welcome_direct #1st
