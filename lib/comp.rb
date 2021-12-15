926eb0a216208777072878ad071dd7f98c647cd5
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-15 16:07:32 -0500

    Feat: Add elements to intellegent shots. Bugs present

--- lib/comp.rb
+++ lib/comp.rb
@@ -12,10 +12,12 @@ class Computer
     @board = Board.new(length, height)
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
+    @diagonal_num = 1
     @shot_sequence = create_shot_sequence(diagonal_up_array, diagonal_down_array)
     @last_hit = nil
     @next_hit = nil
     @shot_array = []
+    @diagonal_num = 1
   end
 
   def random_coordinate  #4
@@ -72,20 +74,21 @@ class Computer
 
   def diagonal_down_array
     numbers = []
-    @board.height.length.times {|number| numbers << number + 1}
+    @board.height.length.times {|number| numbers << number + @diagonal_num}
     diagonal = @board.height.zip(numbers)
     diagonal.map {|array| array.join}
   end
 
   def diagonal_up_array
     numbers = []
-    @board.length.times {|number| numbers << number + 1}
+    @board.length.times {|number| numbers << number + @diagonal_num}
     diagonal = @board.height.reverse.zip(numbers)
     diagonal.map {|array| array.join}
   end
 
   def create_shot_sequence(array_1, array_2)
     array_1.each {|coord| array_2 << coord}
+    array_2.map {|coord| array_2.delete(coord) if @board.cells.keys.include?(coord) == false}
     array_2
   end
 
@@ -93,18 +96,19 @@ class Computer
     ord = @last_hit[0].ord + 1
     @shot_array = ["#{ord.chr}#{@last_hit[-1]}", "#{(ord - 2).chr}#{@last_hit[-1]}"]
     @shot_array.map {|coord| @shot_array.delete(coord) if board.cells.keys.include?(coord) == false}
-    kill_shots(board)
+    @shot_array
+    @shot_array.length == 0 ? start_intelligent_shots(board) : kill_shots(board)
   end
 
   def kill_shots(board)
     if board.cells[@shot_array[0]].empty? == false && board.cells[@shot_array[0]].ship.sunk? == true
       @last_hit = nil
       intelligent_attack(board)
-    elsif board.cells[@shot_array[0]].fired_upon? == false
+    elsif board.cells.keys.include?(@shot_array[0]) == true && board.cells[@shot_array[0]].fired_upon? == false
       board.cells[@shot_array[0]].fire_upon
       @last_hit = @shot_array[0] if board.cells[@shot_array[0]].empty? == false
       @shot_array.shift
-    elsif board.cells[@shot_array[1]].fired_upon? == false
+    elsif board.cells.keys.include?(@shot_array[1]) == true && board.cells[@shot_array[1]].fired_upon? == false
       board.cells[@shot_array[1]].fire_upon
       @last_hit = @shot_array[1] if board.cells[@shot_array[1]].empty? == false
       @shot_array.pop
@@ -117,14 +121,21 @@ class Computer
     num = @last_hit[-1].to_i
     @shot_array = ["#{@last_hit[0]}#{num - 1}", "#{@last_hit[0]}#{num + 1}"]
     @shot_array.map {|coord| @shot_array.delete(coord) if board.cells.keys.include?(coord) == false}
-    kill_shots(board)
+    p @shot_array
+    @shot_array.length == 0 ? start_intelligent_shots(board) : kill_shots(board)
   end
 
   def start_intelligent_shots(board)
-    shot = @shot_sequence.shuffle!.first
-    p "look here #{shot} in #{@shot_sequence}"
-    board.cells[shot].fire_upon if board.cells.keys.include?(shot) == true
-    @last_hit = shot if board.cells[shot].empty? == false
+    p @shot_sequence
+    if @shot_sequence.length == 0
+      @shot_sequence = board.cells.values.select {|cell| cell.shot_at == false}
+      @shot_sequence = @shot_sequence.map {|cell| cell.coordinate}
+    end
+    shot = @shot_sequence.shuffle.first
+    board.cells[shot].fire_upon if board.cells.keys.include?(shot) == true  ##can refactor these next two
+    if board.cells.keys.include?(shot) == true && board.cells[shot].empty? == false
+      @last_hit = shot
+    end
     @shot_sequence.delete(shot)
   end
 
@@ -132,10 +143,8 @@ class Computer
     p "last hit - #{@last_hit}"
     if @last_hit == nil
       start_intelligent_shots(board)
-    elsif @last_hit != nil
+    else @last_hit != nil
       horizontal_sink_pattern(board)
-    else
-      puts "Yay"
     end
   end
 end
@@ -154,6 +163,7 @@ end
 #p computer_board.diagonal_down_array
 #p computer_board.diagonal_up_array
 #p computer_board.shot_sequence
+#p computer_board.intelligent_attack(user_board)
 
 
 
