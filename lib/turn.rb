de3665d909647dbfce84beb583e336c5f2052531
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-15 16:31:48 -0500

    Fix: fix bug in fire_upon method and remove comments

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,4 @@
-require "./lib/ship"
-require "./lib/cell"
-require "./lib/board"
 require "./lib/comp"
-
 
 class Turn
   attr_reader :play,
@@ -21,33 +17,31 @@ class Turn
     @comp_shot = comp_shot
   end
 
-  def setup #2nd
+  def setup
     @user_cruiser   = Ship.new("Cruiser", 3)
     @user_submarine = Ship.new("Submarine", 2)
     @comp_cruiser   = Ship.new("Cruiser", 3)
     @comp_submarine = Ship.new("Submarine", 2)
     @computer       = Computer.new
     @user_board     = Board.new
-
-    @computer.assign_to_cells(@comp_cruiser) #Random assignment
-    @computer.assign_to_cells(@comp_submarine) #Random assignment
-    #
+    @computer.assign_to_cells(@comp_cruiser)
+    @computer.assign_to_cells(@comp_submarine)
   end
 
-  def welcome_direct #1st
+  def welcome_direct
     puts "Welcome to BATTLESHIP"
-    puts "Enter p to play. Enter q to quit."
+    puts "Enter q to quit. Enter any other key to play"
     @play = gets.chomp
   end
 
-  def place_direct #3rd
+  def place_direct
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
-    puts "The Cruiser is three units long and the Submarine is two units long."#concatenate later
+    puts "The Cruiser is three units long and the Submarine is two units long."
   end
 
-  def user_place(ship) #will need to run 2x in runner (once per ship)
-    puts @user_board.render(true) #show board
+  def user_place(ship)
+    puts @user_board.render(true)
     puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
     coordinates = gets.chomp.split(' ')
     while @user_board.valid_placement?(ship, coordinates) == false
@@ -61,10 +55,10 @@ class Turn
     puts "=============COMPUTER BOARD============="
     puts @computer.board.render
     puts "==============PLAYER BOARD=============="
-    puts @user_board.render(true) #Why is this returning nil
+    puts @user_board.render(true)
   end
 
-  def user_shot #1st
+  def user_shot
     valid = false
     puts "Enter the coordinate for your shot:"
     @shot = gets.chomp
@@ -79,10 +73,10 @@ class Turn
   end
 
   def valid_shot?(coordinate)
-    @computer.board.cells[coordinate].fired_upon? == false && @computer.board.valid_coordinate?(coordinate) == true
+    @computer.board.valid_coordinate?(coordinate) == true && @computer.board.cells[coordinate].fired_upon? == false
   end
 
-  def computer_shot #2nd #computer's shot is only invalid if repeated
+  def computer_shot
     valid = false
     while valid == false
       @comp_shot = @computer.random_coordinate
@@ -124,7 +118,6 @@ class Turn
     place_direct
     user_place(@user_cruiser)
     user_place(@user_submarine)
-
     game_on = true
     while game_on == true
       show_board
@@ -138,5 +131,3 @@ class Turn
     next_game.game_flow
   end
 end
-turn = Turn.new
-turn.game_flow
